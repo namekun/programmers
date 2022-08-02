98a73bc1aacdd6ed8ca5f6b0ce367429cd27d1d9
namhyuk kim <happykimnh@icloud.com>
2022-08-02 22:45:17 +0900

    [2022-08-02] 올바른 괄호

---
+++ src/lv2/올바른괄호.java
@@ -0,0 +1,25 @@
+package lv2;
+
+import java.util.Stack;
+
+public class 올바른괄호 {
+    public static void main(String[] args) {
+//        System.out.println(solution("(())()"));
+        System.out.println(solution(")()("));
+    }
+
+    public static boolean solution(String s) {
+
+        Stack<Character> stack = new Stack<>();
+        char[] charArray = s.toCharArray();
+
+        if(charArray[0] == ')') return false;
+
+        for (char c : charArray) {
+            if (c == '(') stack.push(c);
+            if (stack.size() > 0 && c == ')') stack.pop();
+        }
+
+        return stack.size() == 0;
+    }
+}
