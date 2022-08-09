62cf24a065b463faa676250661ddefe82bf6635a
namhyuk kim <happykimnh@icloud.com>
2022-08-09 22:00:07 +0900

    [2022-08-10] H-Index

---
+++ src/lv2/HINDEX.java
@@ -0,0 +1,24 @@
+package lv2;
+
+import java.util.Arrays;
+
+public class HINDEX {
+    public static void main(String[] args) {
+        int solution = solution(new int[]{3, 0, 5, 1, 5});
+        System.out.println(solution);
+    }
+
+    public static int solution(int[] citations){
+        Arrays.sort(citations);
+        int answer = 0;
+        for(int i = 0; i < citations.length; i ++){
+            int h = citations.length - i;
+
+            if(citations[i] >= h){
+                answer = h;
+                break;
+            }
+        }
+        return answer;
+    }
+}
