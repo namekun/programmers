91beb266a3dab4e0e02b6c0c6c04b4b03622269a
namhyuk kim <happykimnh@icloud.com>
2022-08-21 23:34:56 +0900

    [2022-08-21] 캐시

---
+++ src/lv2/캐시.java
@@ -0,0 +1,38 @@
+package lv2;
+
+import java.util.LinkedList;
+
+public class 캐시 {
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int cachesize, String[] cities) {
+        int answer = 0;
+
+        if(cachesize == 0) return cities.length * 5;
+
+        LinkedList<String> cache = new LinkedList<>();
+
+        for (String city : cities) {
+
+            city = city.toLowerCase();
+
+            if (cache.contains(city)) {
+                cache.remove(city);
+                cache.addFirst(city);
+                answer += 1;
+            } else {
+
+                if (cache.size() == cachesize) {
+                    cache.pollLast();
+                }
+
+                cache.addFirst(city);
+                answer += 5;
+            }
+        }
+
+        return answer;
+    }
+}
