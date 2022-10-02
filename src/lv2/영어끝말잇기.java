30e10fe5c2c7c5a6c71b764256b3aca7402072de
NAMKUN <happykimnh@icloud.com>
2022-10-02 18:02:57 +0900

    [2022-10-02] 최대값과 최솟값

--- src/lv2/영어끝말잇기.java
+++ src/lv2/영어끝말잇기.java
@@ -5,7 +5,7 @@ import java.util.Set;
 
 public class 영어끝말잇기 {
     public static void main(String[] args) {
-        solution()
+
     }
 
     public static int[] solution(int n, String[] words) {
@@ -13,11 +13,7 @@ public class 영어끝말잇기 {
 
         Set<String> used = new HashSet<>();
         for (int i = 0; i < words.length; i++) {
-            if (i > 0 && words[i - 1].charAt(words[i - 1].length() - 1) != words[i].charAt(0)) {
-                return new int[]{(i % n) + 1, (i / n) + 1};
-            }
-
-            if (used.contains(words[i])) {
+            if ((i > 0 && words[i - 1].charAt(words[i - 1].length() - 1) != words[i].charAt(0)) || used.contains(words[i])) {
                 return new int[]{(i % n) + 1, (i / n) + 1};
             }
 
